b6dd00d080c18401fa778c892d06671819ba8390
Volodymyr Shevchenko <volodymyr.shevchenko@corp.kismia.com>
2018-03-18 14:41:36 +0200

    Add dependency injection

---
+++ DependencyInjection/CentrifugoExtension.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: vladimir
+ * Date: 15.03.18
+ * Time: 14:43
+ */
+
+namespace Kismia\CentrifugoBundle\DependencyInjection;
+
+
+use Kismia\CentrifugoBundle\Client\CentrifugoClient;
+use Kismia\CentrifugoBundle\DependencyInjection\Configuration\Configuration;
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Definition;
+use Symfony\Component\DependencyInjection\Extension\Extension;
+use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
+
+class CentrifugoExtension extends Extension
+{
+    /**
+     * @inheritDoc
+     */
+    public function load(array $configs, ContainerBuilder $container)
+    {
+        $configuration = new Configuration();
+        $config = $this->processConfiguration($configuration, $configs);
+
+        $loader = new YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+        $loader->load('services.yml');
+
+        //$defiantion = new Definition(CentrifugoClient::class, [$config['apiendpoint'], $config['secret'], $config['transport']]);
+        //$defiantion->setSynthetic(true);
+
+        //$container->setDefinition('centrifugo_client', $defiantion);
+
+    }
+
+}
---
+++ DependencyInjection/Configuration/Configuration.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: vladimir
+ * Date: 15.03.18
+ * Time: 13:28
+ */
+
+namespace Kismia\CentrifugoBundle\DependencyInjection\Configuration;
+
+
+use Symfony\Component\Config\Definition\Builder\TreeBuilder;
+use Symfony\Component\Config\Definition\ConfigurationInterface;
+
+class Configuration implements ConfigurationInterface
+{
+    public function getConfigTreeBuilder()
+    {
+        $treeBuilder = new TreeBuilder();
+        $rootNode = $treeBuilder->root('centrifugo');
+        $rootNode
+            ->children()
+                ->scalarNode('apiendpoint')->isRequired()->end()
+                ->scalarNode('secret')->isRequired()->end()
+                ->arrayNode('transport')
+                    ->children()
+                        ->arrayNode('redis')
+                            ->children()
+                                ->scalarNode('host')->isRequired()->end()
+                                ->integerNode('port')->defaultValue(6379)->end()
+                                ->integerNode('shards')->isRequired()->end()
+                                ->integerNode('db')->defaultValue(0)->end()
+                                ->floatNode('timeout')->defaultValue(0.3)->end()
+                            ->end()
+                        ->end()
+                        ->arrayNode('http')
+                            ->canBeUnset()
+
+                        ->end()
+                    ->end()
+                ->end()
+            ->end();
+
+        return $treeBuilder;
+
+    }
+
+}
